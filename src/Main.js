ccb721a10575368ec97b6a85eb8c22140f0a4b7e
jazreeeel <jazreel.kwek@gmail.com>
2020-03-29 20:56:38 +0800

    'constraints'

--- src/Main.js
+++ src/Main.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import loadInputs from './pages/loadInputs';
-import form from './form/form';
+import createAccount from './pages/createAccount';
 
 const Main =() => {
     return (
       <Switch>
         <Route exact path='/' component={Home}></Route>
-        <Route exact path='/loadInputs' component={form}></Route>
+        <Route exact path='/loadInputs' component={loadInputs}></Route>
+        <Route exact path='/createAccount' component={createAccount}></Route>
       </Switch>
     );
   }
